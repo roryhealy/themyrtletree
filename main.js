287c588ec62568e543c646a93b425bc34dd9e321
Rory Healy <roryihealy@outlook.com>
2023-04-18 03:33:53 +1000

    Add audio play/stop button

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
 const backgroundAudio = document.getElementById("backgroundAudio");
+const playPauseButton = document.getElementById("playPause")
+let audioPlaying = false
 
-const playAudio = () => {
-  backgroundAudio.play();
-};
-
-const pauseAudio = () => {
-  backgroundAudio.pause();
+const playPauseAudio = () => {
+  if (audioPlaying === true) {
+    playPauseButton.innerHTML = playPauseButton.innerHTML.replace('pause', 'play');
+    backgroundAudio.pause();
+    audioPlaying = false;
+  } else {
+    playPauseButton.innerHTML = playPauseButton.innerHTML.replace('play', 'pause');
+    backgroundAudio.play();
+    audioPlaying = true;
+  }
 };
 
 const stopAudio = () => {
-  backgroundAudio.pause();
   backgroundAudio.currentTime = 0;
+  audioPlaying = true;
+  playPauseAudio();
+};
+
+const begin = () => {
+  // First pause the audio
+  playPauseAudio();
+
+  // Then fade everything out
+
+  // Finally, load in the new content
 };
